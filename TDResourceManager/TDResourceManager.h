d3d5ef2b0d6882a829f450fd1fc6de92dfc1eeb7
Robin Hsu <robinhsu599+dev@gmail.com>
2015-04-25 19:27:22 +0800

    Initial Commit

---
+++ TDResourceManager/TDResourceManager.h
@@ -0,0 +1,13 @@
+//
+//  TDResourceManager.h
+//  TDResourceManager
+//
+//  Created by Robin Hsu on 2015/4/25.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TDResourceManager : NSObject
+
+@end
---
+++ TDResourceManager/TDResourceManager.m
@@ -0,0 +1,13 @@
+//
+//  TDResourceManager.m
+//  TDResourceManager
+//
+//  Created by Robin Hsu on 2015/4/25.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import "TDResourceManager.h"
+
+@implementation TDResourceManager
+
+@end
