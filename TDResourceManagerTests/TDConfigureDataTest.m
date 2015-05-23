e4995b79aa9d496b9eec65d3e6f8941839fda51f
Robin Hsu <robinhsu599+dev@gmail.com>
2015-05-23 23:18:24 +0800

    add unit test for file type is Property List.

--- TDResourceManagerTests/TDConfigureDataTest.m
+++ TDResourceManagerTests/TDConfigureDataTest.m
@@ -217,12 +217,147 @@
                                                                     from: fullPath inZippedPath: @"Resources" with: nil];
     XCTAssertTrue( result, @"method's result should be true." );
     
+}
+
+//  ------------------------------------------------------------------------------------------------
+- ( void ) testGetConfigureFromDefaultWithPlist
+{
+    [self                           copyResourceData];
+    
+    configureData                   = [TDConfigureData loadConfigureData: @"StickerLibraryTabDefault"
+                                                                    type: TDConfigureDataSourceFileTypePList encoding: NSUTF8StringEncoding
+                                                           withConfigure: @"Tab"
+                                                                    from: TDTemporaryDirectory inDirectory: @"Tester/Resources" onSingleton: NO];
+    XCTAssertNotNil( configureData, @"Resource manager should not be nil" );
+    
+    XCTAssertNotNil( [configureData configureData], @"configure Data container should not be nil" );
+    NSLog( @"container : %@", [configureData configureData] );
+    
+    BOOL                            result;
+    
+    result                          = [configureData updateConfigureData: @"StickerLibraryTabUpdate"
+                                                                    type: TDConfigureDataSourceFileTypePList encoding: NSUTF8StringEncoding
+                                                           withConfigure: @"Tab" and: @"Name"
+                                                                    from: TDTemporaryDirectory inDirectory: @"Tester/Resources"];
+    XCTAssertTrue( result, @"method's result should be true." );
+    XCTAssertNotNil( [configureData configureData], @"configure Data container should not be nil" );
+    NSLog( @"container : %@", [configureData configureData] );
+}
+
+//  ------------------------------------------------------------------------------------------------
+- ( void ) testGetConfigureFromAssetsBundleWithPlist
+{
+    [self                           copyResourceData];
+    
+    configureData                   = [TDConfigureData loadConfigureData: @"StickerLibraryTabDefault"
+                                                                    type: TDConfigureDataSourceFileTypePList encoding: NSUTF8StringEncoding
+                                                           withConfigure: @"Tab"
+                                                                    from: @"Tester/JSQMATest.bundle" with: [self class] inDirectory: @"Resources" onSingleton: NO];
+    XCTAssertNotNil( configureData, @"Resource manager should not be nil" );
+    
+    XCTAssertNotNil( [configureData configureData], @"configure Data container should not be nil" );
+    NSLog( @"container : %@", [configureData configureData] );
+    
+    BOOL                            result;
+    
+    result                          = [configureData updateConfigureData: @"StickerLibraryTabUpdate"
+                                                                    type: TDConfigureDataSourceFileTypePList encoding: NSUTF8StringEncoding
+                                                           withConfigure: @"Tab" and: @"Name"
+                                                                    from: @"Tester/JSQMATest.bundle" with: [self class] inDirectory: @"Resources"];
+    XCTAssertTrue( result, @"method's result should be true." );
+    XCTAssertNotNil( [configureData configureData], @"configure Data container should not be nil" );
+    NSLog( @"container : %@", [configureData configureData] );
     
 }
 
+//  ------------------------------------------------------------------------------------------------
+- ( void ) testGetConfigureFromZippedFullPathWithPlist
+{
+    [self                           copyResourceData];
+    
+    //  zipped.
+    NSString                      * fullPath;
+    NSBundle                      * bundle;
+    NSString                      * bundleResourcePath;
+    
+    bundle                          = [NSBundle bundleForClass: [self class]];
+    XCTAssertNotNil( bundle, @"the bundle should not b nil" );
+    bundleResourcePath              = [bundle resourcePath];
+    XCTAssertNotNil( bundleResourcePath, @"the bundle's path should not b nil" );
+    
+    
+    fullPath                        = [bundleResourcePath stringByAppendingPathComponent: @"Tester/Resources.zip"];
+    configureData                   = [TDConfigureData loadConfigureData: @"StickerLibraryTabDefault"
+                                                                    type: TDConfigureDataSourceFileTypePList encoding: NSUTF8StringEncoding
+                                                           withConfigure: @"Tab"
+                                                                    from: fullPath inZippedPath: @"Resources" with: nil onSingleton: NO];
+    XCTAssertNotNil( configureData, @"Resource manager should not be nil" );
+    XCTAssertNotEqualObjects( configureData, [TDConfigureData defaultManager], @"both object should not be equaled" );
+    
+    configureData                   = [TDConfigureData loadConfigureData: @"StickerLibraryTabDefault"
+                                                                    type: TDConfigureDataSourceFileTypePList encoding: NSUTF8StringEncoding
+                                                           withConfigure: @"Tab"
+                                                                    from: fullPath inZippedPath: @"Resources" with: nil onSingleton: YES];
+    XCTAssertNotNil( configureData, @"Resource manager should not be nil" );
+    XCTAssertEqualObjects( configureData, [TDConfigureData defaultManager], @"both object should be equaled" );
+    XCTAssertNotNil( [configureData configureData], @"configure Data container should not be nil" );
+    NSLog( @"container : %@", [configureData configureData] );
+    
+}
 
 //  ------------------------------------------------------------------------------------------------
-
+- ( void ) testGetConfigureFromZippedWithPlist
+{
+    [self                           copyResourceData];
+    
+    //  zipped.
+    BOOL                            result;
+    
+    configureData                   = [TDConfigureData loadConfigureData: @"StickerLibraryTabDefault"
+                                                                    type: TDConfigureDataSourceFileTypePList encoding: NSUTF8StringEncoding
+                                                           withConfigure: @"Tab"
+                                                                    from: @"Resources" forDirectories: TDTemporaryDirectory inDirectory: @"Tester"
+                                                            inZippedPath: @"Resources" with: nil onSingleton: NO];
+    XCTAssertNotNil( configureData, @"Resource manager should not be nil" );
+    XCTAssertNotEqualObjects( configureData, [TDConfigureData defaultManager], @"both object should not be equaled" );
+    
+    
+    configureData                   = [TDConfigureData loadConfigureData: @"StickerLibraryTabDefault"
+                                                                    type: TDConfigureDataSourceFileTypePList encoding: NSUTF8StringEncoding
+                                                           withConfigure: @"Tab"
+                                                                    from: @"Resources" forDirectories: TDTemporaryDirectory inDirectory: @"Tester"
+                                                            inZippedPath: @"Resources" with: nil onSingleton: YES];
+    XCTAssertNotNil( configureData, @"Resource manager should not be nil" );
+    XCTAssertEqualObjects( configureData, [TDConfigureData defaultManager], @"both object should be equaled" );
+    XCTAssertNotNil( [configureData configureData], @"configure Data container should not be nil" );
+    NSLog( @"container : %@", [configureData configureData] );
+    
+    result                          = [configureData updateConfigureData: @"StickerLibraryTabUpdate"
+                                                                    type: TDConfigureDataSourceFileTypePList encoding: NSUTF8StringEncoding
+                                                           withConfigure: @"Tab" and: @"Name"
+                                                                    from: @"Resources" forDirectories: TDTemporaryDirectory inDirectory: @"Tester"
+                                                            inZippedPath: @"Resources" with: nil];
+    XCTAssertTrue( result, @"method's result should be true." );
+    XCTAssertNotNil( [configureData configureData], @"configure Data container should not be nil" );
+    NSLog( @"container : %@", [configureData configureData] );
+    
+    NSString                      * fullPath;
+    NSBundle                      * bundle;
+    NSString                      * bundleResourcePath;
+    
+    bundle                          = [NSBundle bundleForClass: [self class]];
+    XCTAssertNotNil( bundle, @"the bundle should not b nil" );
+    bundleResourcePath              = [bundle resourcePath];
+    XCTAssertNotNil( bundleResourcePath, @"the bundle's path should not b nil" );
+    
+    
+    fullPath                        = [bundleResourcePath stringByAppendingPathComponent: @"Tester/Resources.zip"];
+    result                          = [configureData updateConfigureData: @"StickerLibraryTabUpdate" type: TDConfigureDataSourceFileTypePList encoding: NSUTF8StringEncoding
+                                                           withConfigure: @"Tab" and: @"Name"
+                                                                    from: fullPath inZippedPath: @"Resources" with: nil];
+    XCTAssertTrue( result, @"method's result should be true." );
+    
+}
 
 //  --------------------------------
 //  ------------------------------------------------------------------------------------------------
@@ -230,3 +365,19 @@
 
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
